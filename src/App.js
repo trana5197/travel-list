ca3658e5c08183143d1e44b5f044707d3a300463
rana <trana51997@gmail.com>
2023-06-13 15:51:20 -0700

    state added and form submitted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 23, description: "Charger", quantity: 1, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -20,21 +22,45 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleDescriptionChange(e) {
+    setDescription(e.target.value);
+  }
+
+  function handleQuantityCahnge(e) {
+    setQuantity(Number(e.target.value));
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newData = { description, quantity, packed: false, id: Date.now() };
+    console.log(newData);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={handleQuantityCahnge}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={handleDescriptionChange}
+      />
       <button>Add</button>
     </form>
   );
@@ -56,7 +82,7 @@ function Item({ item }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {initialItems.quantity} {item.description}
+        {item.quantity} {item.description}
         <button>❌</button>
       </span>
     </li>
